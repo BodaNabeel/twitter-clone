71b547229e588f167a7cf20b2ea460015e72f2ed
Nabeel Boda <needtoexplore01@gmail.com>
2022-04-13 00:54:18 +0530

    Drawer styled

--- src/components/MUIDrawer.jsx
+++ src/components/MUIDrawer.jsx
@@ -1,65 +1,65 @@
 import {
-  Divider,
   Drawer,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
-  Toolbar,
 } from "@mui/material";
-
-import HomeIcon from '@mui/icons-material/Home';
-import TagIcon from '@mui/icons-material/Tag';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import ListIcon from '@mui/icons-material/List';
-import PermIdentityIcon from '@mui/icons-material/PermIdentity';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import HomeIcon from "@mui/icons-material/Home";
+import TagIcon from "@mui/icons-material/Tag";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListIcon from "@mui/icons-material/List";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import CreateTweet from "./CreateTweet";
 export default function MUIDrawer() {
-    const drawerInfo = [
-        {
-            text: "Home",
-            icon: <HomeIcon/>
-        },
-        {
-            text: "Explore",
-            icon: <TagIcon/>
-        },
-        {
-            text: "Notifications",
-            icon: <NotificationsNoneIcon/>
-        },
-        {
-            text: "Messages",
-            icon: <MailOutlineIcon/>
-        },
-        {
-            text: "Bookmarks",
-            icon: <BookmarkBorderIcon/>
-        },
-        {
-            text: "Lists",
-            icon: <ListIcon/>
-        },
-        {
-            text: "Profiles",
-            icon: <PermIdentityIcon/>
-        },
-        {
-            text: "More",
-            icon: <MoreHorizIcon/>
-        },
-    ]
+  const drawerInfo = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "Explore",
+      icon: <TagIcon />,
+    },
+    {
+      text: "Notifications",
+      icon: <NotificationsNoneIcon />,
+    },
+    {
+      text: "Messages",
+      icon: <MailOutlineIcon />,
+    },
+    {
+      text: "Bookmarks",
+      icon: <BookmarkBorderIcon />,
+    },
+    {
+      text: "Lists",
+      icon: <ListIcon />,
+    },
+    {
+      text: "Profiles",
+      icon: <PermIdentityIcon />,
+    },
+    {
+      text: "More",
+      icon: <MoreHorizIcon />,
+    },
+  ];
   const drawer = (
-    <div>
+    <>
       <ListItemIcon
-      sx={{
-          margin: "20px"
-      }}
-      ><TwitterIcon/></ListItemIcon>
-      <Divider />
+        sx={{
+          margin: "20px",
+          color: "#1D9BF0",
+        }}
+      >
+        <TwitterIcon />
+      </ListItemIcon>
       <List>
         {drawerInfo.map((el) => (
           <ListItem button key={el.text}>
@@ -68,16 +68,19 @@ export default function MUIDrawer() {
           </ListItem>
         ))}
       </List>
-    </div>
+      <CreateTweet />
+      </>
   );
   return (
-    <Drawer
-      open
-      variant="permanent"
-      sx={{
-        width: 240,
-      }}
-    >
+    <Drawer 
+    PaperProps={{
+        sx: {
+            width: 240,
+            ml:5,
+        }
+    }}
+    open 
+    variant="permanent">
       {drawer}
     </Drawer>
   );
